c69f12c552605e9f4df364f20c6abdaa64449992
Lorenzo Carducci <card.lorenzo@gmail.com>
2019-08-30 16:29:14 +0200

    Add Tracker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CronometerComponent } from './cronometer/cronometer.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
 const appRoutes: Routes = [
   { path: 'cronometer', component: CronometerComponent },
+  { path: 'tracker', component: TrackerComponent },
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    CronometerComponent
+    CronometerComponent,
+    TrackerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tracker/tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker',
+  templateUrl: './tracker.component.html',
+  styleUrls: ['./tracker.component.sass']
+})
+export class TrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
